2acb1f92cdea586bbedb4953f5f911e57f332c57
twinzies <kjaintara@gmail.com>
2022-12-03 21:19:52 +0530

    Add function definitions Simplify fractional property interface Decouple property from fractional object Depracate property object from v1

--- LandsDao.sol
+++ LandsDao.sol
@@ -1,89 +1,112 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4;
+// SPDX-License-Identifier: Unlicensed
 
-import '@openzeppelin/contracts/utils/Counters.sol';
-// import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
-// import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-// import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+pragma solidity 0.8.4;
 
-//import hardhat/console.sol
-
-pragma solidity >=0.4.0 < 0.9.0;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract LandsDAO {
+
+    uint256 public propertyCounter;
     address payable owner;
-    using Counters for Counters.Counter;
-
     uint256 public listingPrice;
-    Counters.Counter private _propertyIds;
-    struct Property {
-        uint256 propertyId;
-        uint[] fractionalIds; // The fractionals that maketh the property.
-        string location;
-        string name;
-        string imageHash; // TODO How to use IPFS cid here?
-        uint256 currentValue;
-    }
 
-    // Maps Property IDs to properties.
-    mapping(uint256 => Property) propertyOracle;
-
-    Counters.Counter private _fractionalIds;
-
-    struct Fractional {
-        uint256 parentPropertyId;
+    struct propertyFractional {
         uint256 fractionalId;
+        address ContractAddress;
+        uint256 tokenId;
+        address payable seller;
         address owner;
-        address seller;
-        address contractAddress; //TODO should this be cid ?
-    
-        bool forSale;
-        uint256 price;
+        uint256 value;
+        bool isSold;
     }
 
-    // Maps Fractional IDs to Fractionals. Private to ensure privacy of owners.
-    mapping(uint256 => Fractional) fractionalOracle;
+    mapping(uint256 => propertyFractional) private allFractionals;
 
-    // When a fractional property is listed for sale.
-    event FractionalPropertyListed(
-        uint256 indexed parentPropertyId,
+    event PropertyFractionalListed(
         uint256 indexed fractionalId,
-        address indexed contractAddress,
-        uint256 price,
-        address owner
-    );
-
-    event FractionalSold (
-        uint256 indexed fractionalId,
-        uint256 price,
-        address owner
-    );
-
-    event NewPropertyListed(
-        uint256 current_value,
-        uint256 propertyId,
-        string location,
-        string name
+        address indexed ContractAddress,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 value
     );
 
     constructor() {
+        propertyCounter = 0;
         owner = payable(msg.sender);
         listingPrice = 0.001 ether;
     }
 
-    // Upon listing, the caller will provide the number of fractionals desired.
-    function listNewProperty() public payable {}
+    function listFractional(
+        address ContractAddress,
+        uint256 tokenId,
+        uint256 value
+    ) public payable {
+        require(msg.value == listingPrice, "Must pay the listing price");
+        require(value > 0, "Price must be greater than 0");
 
-    function sellFractional() public payable {}
+        allFractionals[propertyCounter] = propertyFractional(
+            propertyCounter,
+            ContractAddress,
+            tokenId,
+            payable(msg.sender),
+            address(0),
+            value,
+            false
+        );
 
-    function buyFractional() public payable {}
+        IERC721(ContractAddress).transferFrom(
+            msg.sender,
+            address(this),
+            tokenId
+        );
 
-    function getFractionalInfo() public view {}
+        payable(owner).transfer(listingPrice);
 
-    function getAllProperties() public view {}
+        emit PropertyFractionalListed(
+            propertyCounter,
+            ContractAddress,
+            tokenId,
+            msg.sender,
+            address(0),
+            value
+        );
 
-    function getAllAvailable() public view {}
+        propertyCounter += 1;
+    }
 
-    function updateFractionalprice() public {}
+    function buyFractional(uint256 fractionalId) public payable {
+        require(allFractionals[fractionalId].isSold == false, "Property is already sold");
+        require(
+            allFractionals[fractionalId].value == msg.value,
+            "Must offer the correct price"
+        );
 
+        allFractionals[fractionalId].isSold = true;
+        allFractionals[fractionalId].owner = payable(msg.sender);
+
+        IERC721(allFractionals[fractionalId].ContractAddress).transferFrom(
+            address(this),
+            msg.sender,
+            allFractionals[fractionalId].tokenId
+        );
+    }
+
+    function getListing(uint256 fractionalId)
+        public
+        view
+        returns (propertyFractional memory fractional)
+    {
+        fractional = allFractionals[fractionalId];
+    }
+
+    function updateValue(uint256 newValue) public {
+        require(newValue > 0, "Updated value must be greater than 0");
+        require(
+            msg.sender == owner,
+            "Only the owner can change the value"
+        );
+
+        listingPrice = newValue;
+    }
 }
